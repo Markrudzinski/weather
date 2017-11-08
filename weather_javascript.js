616ebcdd219edba8bde1df001fe41b72b385d8b3
Mark Rudzinski <mrudzinski@renewaldenver.local>
2017-11-07 18:17:04 -0800

    add Celc to Fahr, Fahr to Celc button

--- weather_javascript.js
+++ weather_javascript.js
@@ -1,3 +1,5 @@
+var celcius = 0;
+var fahrenheit = 0;
 $(document).ready(function(){
   var location = "https://fcc-weather-api.glitch.me/api/current?";
   if (navigator.geolocation) {
@@ -7,17 +9,27 @@ $(document).ready(function(){
         dataType: "json",
         url: location,
         success: function(data) {
+          celcius = Math.round(data.main.temp * 10) / 10;
           $("#city").html(data.name + ", " + data.sys.country);
-          $("#temp").html(Math.round(data.main.temp * 10) / 10 + " &deg");
+          $("#temp").html(celcius + " &deg ");
           $("#description").html(data.weather[0].main);
           $("#weather_img").attr("src", data.weather[0].icon);
-          console.log(data.weather[0].icon);
+          $("#temp").append('<button class="buttonTemp" onclick="toFahrenheit(this)" >C</button>');
         }
       });
     });
   }
 });
-
+function toFahrenheit() {
+  fahrenheit = Math.round((celcius * 1.8 + 32) * 10) / 10;
+  $("#temp").html(fahrenheit + " &deg ");
+  $("#temp").append('<button class="buttonTemp" onclick="toCelcius(this)" >F</button>');
+}
+function toCelcius() {
+  celcius = Math.round(((fahrenheit - 32) / 1.8) * 10) / 10;
+  $("#temp").html(celcius + " &deg ");
+  $("#temp").append('<button class="buttonTemp" onclick="toFahrenheit(this)" >C</button>');
+}
 
 //  This is a sample data structure from https://fcc-weather-api.glitch.me/api/current?
 // { "coord" :{"lon":-122.26,"lat":37.85},
